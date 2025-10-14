57988422d311c508a7ae184c00b43dd03bd90c61
Mahan Kiyani <mahankiyani.rd@gmail.com>
2025-10-14 13:21:56 +0330

    basic python code for beginner

--- basic/1.py
+++ basic/1.py
@@ -1,3 +1,8 @@
 # pylint: disable = c0304
 
-your_feel = input('happy or sad?')
+your_feel = input('happy or sad?').lower()
+
+if your_feel == 'happy':
+    print('your life is very good')
+else:
+    print('your Life is very bad')
