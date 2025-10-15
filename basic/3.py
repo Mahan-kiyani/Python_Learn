c364db5ea5157c448f205eef2173cb23b146368d
Mahan Kiyani <mahankiyani.rd@gmail.com>
2025-10-15 16:10:32 +0330

    test code :)

--- basic/3.py
+++ basic/3.py
@@ -1,2 +1,26 @@
 # simple Calculator, Python code
 
+num_1 = int(input('Enter number: '))
+num_2 = int(input('Enter number: '))
+
+operation = input('Enter your operation: ')
+
+while True:
+    
+    match operation:
+        case '+':
+            reslt = num_1 + num_2
+        
+        case '-':
+            reslt = num_1 - num_2
+            
+        case '*':
+            reslt = num_1 * num_2
+        case '/':
+            reslt = num_1 / num_2
+            
+    break 
+
+
+if isinstance(reslt, int):
+    print(reslt)
