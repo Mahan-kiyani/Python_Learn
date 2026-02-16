2310f869ebf5877a636c8fab86aa5ff287ac37e4
Mahan <mahankiyani.rd@gmail.com>
2026-02-17 00:10:14 +0330

    New Project: Basic Python update and test project

--- BASIC_Python/5.py
+++ BASIC_Python/5.py
@@ -1,11 +1,28 @@
 #Temperature Converter
 
-def c_f(celsius):
-    
+def c_f(celsius: float) -> float:
     return (1.8 * celsius) + 32
 
 
-def f_c(fahrenheit):
-    
-    return (32 - fahrenheit) * (5 / 9)
+def f_c(fahrenheit: float) -> float:
+    return (fahrenheit - 32) * (5 / 9)
 
+
+choice = int(input('''\
+1)fahrenheit to celsius
+2)selcius to fahrenheit
+
+what do you want:
+
+'''))
+
+if choice == 1:
+    degree = float(input('Fahrenheit degree: '))
+    print(f_c(degree))
+    
+elif choice == 2:
+    degree = float(input('Celsius degree: '))
+    print(c_f(degree))
+    
+else: 
+    print('Error!!!\n Your Answer is incorrect')
