59524f7bfa7e65af09f30d28831159d635d15221
Mahan kiyani <mahankiyani.rd@gmail.com>
2026-06-03 23:12:37 +0330

    Update: new python project polymorph in python sabzlearn

---
+++ SABZLEARN_Python/10/polymorph.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        
+    def info
+class Car(Vehicle):
+    pass
+
+class Airplane(Vehicle):
+    pass
+
+class Bicycle(Vehicle):
+    pass
+
+
+class Lamborghini(Car):
+    pass
+
+class Benz(Car):
+    pass
